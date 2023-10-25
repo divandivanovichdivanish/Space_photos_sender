7219844d2a221bf7cb52100c8c8878ffb0329a83
divandivanovichdivanish <120028001+divandivanovichdivanish@users.noreply.github.com>
2023-10-25 17:52:44 +0300

    publish photo to Telegram channel

--- bot.py
+++ bot.py
@@ -6,4 +6,4 @@ from dotenv import load_dotenv
 load_dotenv()
 telegram_token = os.environ["TG_TOKEN"]
 bot = telegram.Bot(token=telegram_token)
-bot.send_message(chat_id="@beautifulphotosofspace", text="This is a test message")
+bot.send_document(chat_id="@beautifulphotosofspace", document=open('images/hubble.jpeg', 'rb'))
